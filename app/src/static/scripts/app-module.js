95cf153f7aeb5b9d7b912a2f89da33b2eeb0f01c
WilberTian <tianli917@hotmail.com>
2016-01-26 14:00:39 +0800

    update app-module.js to remove app variable

--- app/src/static/scripts/app-module.js
+++ app/src/static/scripts/app-module.js
@@ -1,5 +1,3 @@
-var app = 
-	
 angular
 	.module("remember", ["ngRoute", "remember.task", "remember.category", 
     "remember.tag", "remember.note", "remember.attachment", "remember.directives", "remember.common"]);
